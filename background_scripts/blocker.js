60a24c303cf62b4c77a91b325549419bfbbfa289
John Mancini <johnmancini30@gmail.com>
2021-12-30 00:28:02 -0500

    done for the day

---
+++ background_scripts/blocker.js
@@ -0,0 +1,26 @@
+/**
+ * CSS to hide everything on the page,
+ * except for elements that have the "beastify-image" class.
+ */
+const hidePage = `body > : {
+    display: none;
+  }`;
+
+
+/**
+ * When a tab is loaded, check if the plugin is on. If the plugin is on, check if the url matches the page.
+ * If it does, block it
+ */
+browser.tabs.onActivated.addListener(function (activeInfo) {
+    browser.storage.local.get("onOff")
+        .then((result) => {
+            if (Object.entries(result).length === 0 || !result.onOff.value) {
+                return;
+            }
+            browser.tabs.query({ currentWindow: true, active: true }).then((tabs) => {
+                let tab = tabs[0];
+                console.log(tab.url);
+                browser.tabs.insertCSS({code: hidePage})
+            }, console.error)
+        });
+});
--- popup/blockit.js
+++ popup/blockit.js
@@ -1,12 +1,5 @@
 import { selectOnOffButton, updateOnOffButton } from "./onOff.js";
 
-/**
- * CSS to hide everything on the page,
- * except for elements that have the "beastify-image" class.
- */
-const hidePage = `body > :not(.beastify-image) {
-    display: none;
-  }`;
 
 /**
 * Listen for clicks on the buttons, and send the appropriate message to
@@ -14,36 +7,6 @@ const hidePage = `body > :not(.beastify-image) {
 */
 function listenForClicks() {
     document.addEventListener("click", (e) => {
-
-        /**
-        * Given the name of a beast, get the URL to the corresponding image.
-        */
-        function beastNameToURL(beastName) {
-            switch (beastName) {
-                case "Frog":
-                    return browser.runtime.getURL("beasts/frog.jpg");
-                case "Snake":
-                    return browser.runtime.getURL("beasts/snake.jpg");
-                case "Turtle":
-                    return browser.runtime.getURL("beasts/turtle.jpg");
-            }
-        }
-
-        /**
-        * Insert the page-hiding CSS into the active tab,
-        * then get the beast URL and
-        * send a "beastify" message to the content script in the active tab.
-        */
-        function beastify(tabs) {
-            browser.tabs.insertCSS({ code: hidePage }).then(() => {
-                let url = beastNameToURL(e.target.textContent);
-                browser.tabs.sendMessage(tabs[0].id, {
-                    command: "beastify",
-                    beastURL: url
-                });
-            });
-        }
-
         function setOnOff(tabs) {
             selectOnOffButton(e.target.textContent);
             updateOnOffButton(e.target.textContent);
@@ -52,20 +15,20 @@ function listenForClicks() {
         /**
         * Remove the page-hiding CSS from the active tab,
         * send a "reset" message to the content script in the active tab.
-        */
+        
         function reset(tabs) {
             browser.tabs.removeCSS({ code: hidePage }).then(() => {
                 browser.tabs.sendMessage(tabs[0].id, {
                     command: "reset",
                 });
             });
-        }
+        }*/
 
         /**
         * Just log the error to the console.
         */
         function reportError(error) {
-            console.error(`Could not beastify: ${error}`);
+            console.error(`Could not blockit; ${error}`);
         }
 
         /**
@@ -75,15 +38,6 @@ function listenForClicks() {
         if (e.target.classList.contains("onOff")) {
             browser.tabs.query({ active: true, currentWindow: true })
                 .then(setOnOff)
-                .catch(reportError);
-        }
-        else if (e.target.classList.contains("reset")) {
-            browser.tabs.query({ active: true, currentWindow: true })
-                .then(reset)
-                .catch(reportError);
-        } else if (e.target.classList.contains("beast")) {
-            browser.tabs.query({ active: true, currentWindow: true })
-                .then(beastify)
                 .catch(reportError);
         }
     });
@@ -96,14 +50,8 @@ function listenForClicks() {
 function reportExecuteScriptError(error) {
     document.querySelector("#popup-content").classList.add("hidden");
     document.querySelector("#error-content").classList.remove("hidden");
-    console.error(`Failed to execute beastify content script: ${error.message}`);
+    console.log(`Failed to execute beastify content script: ${error.message}`);
 }
-
-/**
-* When the popup loads, inject a content script into the active tab,
-* and add a click handler.
-* If we couldn't inject the script, handle the error.
-*/
 
 function onStartUp() {
     browser.storage.local.get("onOff")
@@ -119,20 +67,3 @@ function onStartUp() {
 
 onStartUp();
 listenForClicks();
-
-/**
- * When a tab is loaded, check if the plugin is on. If the plugin is on, check if the url matches the page.
- * If it does, block it
- */
-browser.tabs.onActivated.addListener(function (activeInfo) {
-    browser.storage.local.get("onOff")
-        .then((result) => {
-            if (Object.entries(result).length === 0 || !result.onOff.value) {
-                return;
-            }
-            browser.tabs.query({currentWindow: true, active: true}).then((tabs) => {
-                let tab = tabs[0]; // Safe to assume there will only be one result
-                console.log(tab.url);
-            }, console.error)
-        });
-});
