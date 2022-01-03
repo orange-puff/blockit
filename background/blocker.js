cb9c846c20dd67c75da98ff5a47bc66fe9dd6840
John Mancini <johnmancini30@gmail.com>
2022-01-02 19:52:02 -0500

    done

--- background/blocker.js
+++ background/blocker.js
@@ -7,50 +7,74 @@ const HIDE_PAGE = `body {
     display: none;
   }`;
 
-/**
- * When a tab is loaded, check if the plugin is on. If the plugin is on, check if the url matches the page.
- * If it does, block it
- */
-browser.tabs.onUpdated.addListener(function (activeInfo) {
-    browser.storage.local.get("onOff")
-        .then((result) => {
-            if (Object.entries(result).length === 0 || !result.onOff.value) {
-                browser.tabs.removeCSS({ code: HIDE_PAGE });
-                return;
-            }
-            browser.tabs.query({ currentWindow: true, active: true })
-                .then((tabs) => {
-                    let tabUrl = tabs[0].url;
-                    browser.storage.local.get("blockedList")
-                        .then((blockedListMeta) => {
-                            if (Object.entries(blockedListMeta).length === 0 ) {
-                                return;
-                            }
-                            let shouldBlock = false;
-                            blockedListMeta.blockedList.value.forEach(url => {
-                                shouldBlock |= tabUrl.includes(url);
-                            });
-                            if (shouldBlock) {
-                                browser.tabs.insertCSS({ code: HIDE_PAGE });
-                            }
-                        });
+function block(on, blockedList) {
+    if (!on || blockedList.length === 0) {
+        browser.tabs.removeCSS({ code: HIDE_PAGE });
+        return;
+    }
 
-                }, console.error);
+    browser.tabs.query({ currentWindow: true, active: true })
+        .then((tabs) => {
+            let tabUrl = tabs[0].url;
+            let shouldBlock = false;
+            blockedList.forEach(url => {
+                shouldBlock |= tabUrl.includes(url);
+            });
+
+            if (shouldBlock) {
+                browser.tabs.insertCSS({ code: HIDE_PAGE });
+            }
+            else {
+                browser.tabs.removeCSS({ code: HIDE_PAGE });
+            }
+        }, console.error);
+}
+
+function getBlockedList(on) {
+    browser.storage.local.get("blockedList")
+        .then(blockedListMeta => {
+            block(on, blockedListMeta.blockedList.value);
         });
-});
+}
+
+function getOnOff(blockedList) {
+    browser.storage.local.get("onOff")
+        .then(onOffMeta => {
+            block(onOffMeta.onOff.value, blockedList);
+        });
+}
 
 function handleMessage(request, sender, sendResponse) {
     if (request.messageName === "onOff") {
-        console.log('onOff!');
-        console.log(request);
+        getBlockedList(request.on);
     }
     else if (request.messageName === "blockedList") {
-        console.log('blockedList!');
-        console.log(request);
+        getOnOff(request.blockedList);
     }
     else {
         console.log("Unknown message!");
     }
-  }
-  
-  browser.runtime.onMessage.addListener(handleMessage);
+}
+
+/**
+ * When a tab is updated, check if the plugin is on. If the plugin is on, check if the url matches the page.
+ * If it does, block it
+ */
+ browser.tabs.onUpdated.addListener(function (activeInfo) {
+    browser.storage.local.get("onOff")
+        .then((onOffMeta) => {
+            if (Object.entries(onOffMeta).length === 0) {
+                return;
+            }
+
+            browser.storage.local.get("blockedList")
+                .then((blockedListMeta) => {
+                    if (Object.entries(blockedListMeta).length === 0) {
+                        return;
+                    }
+                    block(onOffMeta.onOff.value, blockedListMeta.blockedList.value);
+                });
+        });
+});
+
+browser.runtime.onMessage.addListener(handleMessage);
