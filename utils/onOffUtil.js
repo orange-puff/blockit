ef196e64595eb83943ec21704b69af1a05684938
John Mancini <johnmancini30@gmail.com>
2022-01-02 20:11:48 -0500

    added better icon

--- utils/onOffUtil.js
+++ utils/onOffUtil.js
@@ -5,6 +5,7 @@ function createOnOff(on) {
     return { onOff: { value: on } };
 }
 
+// change onOff button state
 export function selectOnOffButton(button) {
     let newVal = false;
     switch (button.toLowerCase()) {
@@ -15,14 +16,16 @@ export function selectOnOffButton(button) {
             break;
     }
     // send a message to the `blocker` background script that the on/off button has just been pressed
-
     browser.runtime.sendMessage({
         messageName: "onOff",
         on: newVal
       });
+
+    // set state
     browser.storage.local.set(createOnOff(newVal));
 }
 
+// change onOff button color
 export function updateOnOffButton(button) {
     let onButton = document.getElementById("on");
     let offButton = document.getElementById("off");
